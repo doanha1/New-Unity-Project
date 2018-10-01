683a028e5d4f59ca8455ce552a0f3c1478c77471
Doan Ha <trong.ha@elev.ga.lbs.se>
2018-10-01 10:43:14 +0200

    saker kan gå runt

---
+++ Assets/ColorChanger.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorChanger : MonoBehaviour
+{
+    public SpriteRenderer rend;
+    public Color color;
+    public TrailRenderer trailRend;
+    public float speed = 50f;
+    public float rotationspeed = 360f;
+
+    void Start()
+    {
+        rend.color = color;
+        //transform.position = new Vector3(-5, transform.position.y);
+        //new Color(0, 1, 0);
+    }
+
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Rotate(0, 0, rotationspeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Rotate(0, 0, -rotationspeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(speed * Time.deltaTime, 0, 0, Space.Self);
+            rend.color = Color.red;
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(-speed * Time.deltaTime, 0, 0, Space.Self);
+            rend.color = Color.green;
+            trailRend.startColor = Color.black;
+            trailRend.endColor = Color.yellow;
+        }
+    }
+}
